f2b7abe62a7b0d093dcc787d6913532e735e588e
Sergio Mora <sergiomora823@gmail.com>
2020-08-21 14:29:02 -0500

    #21082020

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
-from distutils.core import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
-  name = 'BannerQuery',         # How you named your package folder (MyLib)
-  packages = ['BannerQuery'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Query from Banner by Ellucian made easy to solve problems and decision making!',   # Give a short description about your library
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author = 'Sergio A. Mora Pardo',                   # Type in your name
-  author_email = 'sergiomora823@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/sergiomora03/BannerQuery',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sergiomora03/BannerQuery/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Banner', 'Query', 'Ellucian', 'Poli', 'SQL', 'Python'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'cx_Oracle',
-          'math',
-          'pandas',
-      ],
-  classifiers=[
+setuptools.setup(
+    name="BannerQuery", # Replace with your own username
+    version="0.0.1",
+    author="Sergio A. Mora Pardo",
+    author_email="sergiomora823@gmail.com",
+    description="Easy tool to do query from Banner by Ellucian to solve problems and decision making!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sergiomora03/BannerQuery",
+    packages=setuptools.find_packages(),
+    classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
@@ -31,4 +23,5 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
+    python_requires='>=3.6',
 )
