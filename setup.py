e1a223f37d8da7df0a60036610eb53675867d979
Sergio Mora <sergiomora823@gmail.com>
2020-08-21 15:08:54 -0500

    pre-release

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@ setuptools.setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
     python_requires='>=3.6',
